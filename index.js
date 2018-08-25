2b2e1a20b2de5798564c4781ccfd85b48cd1bc6c
matfax <matthias.fax@gmail.com>
2018-08-25 14:46:44 +0200

    Fix conditional build on pull request

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
-const fs = require('fs');
-const path = require('path');
+const fs = require("fs");
+const path = require("path");
 
 // Load docker hub library
-var dockerHubAPI = require('docker-hub-api');
+var dockerHubAPI = require("docker-hub-api");
 
 // Login
 dockerHubAPI.login(process.env.DOCKERHUB_USR, process.env.DOCKERHUB_PWD)
@@ -11,11 +11,10 @@ dockerHubAPI.login(process.env.DOCKERHUB_USR, process.env.DOCKERHUB_PWD)
     dockerHubAPI.setLoginToken(info.token);
 }).then(function () {
     // Load README
-    var filePath = process.env.README_PATH || path.join('/data' , 'README.md');
-    const readme = fs.readFileSync(filePath, {encoding: 'utf-8'});
+    var filePath = process.env.README_PATH || path.join("/data" , "README.md");
+    const readme = fs.readFileSync(filePath, {encoding: "utf-8"});
 
     // Update repository description
-    dockerHubAPI.setRepositoryDescription((process.env.DOCKERHUB_ORG || process.env.DOCKERHUB_USR), (process.env.DOCKERHUB_REPO || process.env.DOCKERHUB_REPO), {full: readme})
+    dockerHubAPI.setRepositoryDescription((process.env.DOCKERHUB_ORG || process.env.DOCKERHUB_USR), (process.env.DOCKERHUB_REPO || process.env.DOCKERHUB_REPO), {full: readme});
 
 });
-
